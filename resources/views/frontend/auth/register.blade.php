7778c492ca8a70a43730fbb8aff71d2bd5c901a5
anandhwebbitech <anandhwebbitech@gmail.com>
2026-04-29 15:03:20 +0530

    state change

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -2191,8 +2191,47 @@
                                             <div class="fiw"><i class="fa-solid fa-map fiw-l"></i>
                                                 <select id="state" name="state" class="finput fc-b">
                                                     <option value="" disabled selected>Select State</option>
-                                                    <option>Tamil Nadu</option>
-                                                    <option>Other</option>
+                                                    <option value="">State</option>
+                                                    <option value="Andhra Pradesh">Andhra Pradesh</option>
+                                                    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                                                    <option value="Assam">Assam</option>
+                                                    <option value="Bihar">Bihar</option>
+                                                    <option value="Chhattisgarh">Chhattisgarh</option>
+                                                    <option value="Goa">Goa</option>
+                                                    <option value="Gujarat">Gujarat</option>
+                                                    <option value="Haryana">Haryana</option>
+                                                    <option value="Himachal Pradesh">Himachal Pradesh</option>
+                                                    <option value="Jharkhand">Jharkhand</option>
+                                                    <option value="Karnataka">Karnataka</option>
+                                                    <option value="Kerala">Kerala</option>
+                                                    <option value="Madhya Pradesh">Madhya Pradesh</option>
+                                                    <option value="Maharashtra">Maharashtra</option>
+                                                    <option value="Manipur">Manipur</option>
+                                                    <option value="Meghalaya">Meghalaya</option>
+                                                    <option value="Mizoram">Mizoram</option>
+                                                    <option value="Nagaland">Nagaland</option>
+                                                    <option value="Odisha">Odisha</option>
+                                                    <option value="Punjab">Punjab</option>
+                                                    <option value="Rajasthan">Rajasthan</option>
+                                                    <option value="Sikkim">Sikkim</option>
+                                                    <option value="Tamil Nadu">Tamil Nadu</option>
+                                                    <option value="Telangana">Telangana</option>
+                                                    <option value="Tripura">Tripura</option>
+                                                    <option value="Uttar Pradesh">Uttar Pradesh</option>
+                                                    <option value="Uttarakhand">Uttarakhand</option>
+                                                    <option value="West Bengal">West Bengal</option>
+
+                                                    <!-- Union Territories -->
+                                                    <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+                                                    <option value="Chandigarh">Chandigarh</option>
+                                                    <option value="Dadra and Nagar Haveli and Daman and Diu">
+                                                        Dadra and Nagar Haveli and Daman and Diu
+                                                    </option>
+                                                    <option value="Delhi">Delhi</option>
+                                                    <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+                                                    <option value="Ladakh">Ladakh</option>
+                                                    <option value="Lakshadweep">Lakshadweep</option>
+                                                    <option value="Puducherry">Puducherry</option>
                                                 </select>
                                             </div>
                                             <div class="ferr-msg" id="e-js_state"><i
@@ -2417,8 +2456,46 @@
                                             <div class="fiw"><i class="fa-solid fa-map fiw-l"></i>
                                                 <select id="c_state" name="c_state" class="finput fc-g">
                                                     <option value="" disabled selected>Select State</option>
-                                                    <option>Tamil Nadu</option>
-                                                    <option>Other</option>
+                                                    <option value="Andhra Pradesh">Andhra Pradesh</option>
+                                                    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                                                    <option value="Assam">Assam</option>
+                                                    <option value="Bihar">Bihar</option>
+                                                    <option value="Chhattisgarh">Chhattisgarh</option>
+                                                    <option value="Goa">Goa</option>
+                                                    <option value="Gujarat">Gujarat</option>
+                                                    <option value="Haryana">Haryana</option>
+                                                    <option value="Himachal Pradesh">Himachal Pradesh</option>
+                                                    <option value="Jharkhand">Jharkhand</option>
+                                                    <option value="Karnataka">Karnataka</option>
+                                                    <option value="Kerala">Kerala</option>
+                                                    <option value="Madhya Pradesh">Madhya Pradesh</option>
+                                                    <option value="Maharashtra">Maharashtra</option>
+                                                    <option value="Manipur">Manipur</option>
+                                                    <option value="Meghalaya">Meghalaya</option>
+                                                    <option value="Mizoram">Mizoram</option>
+                                                    <option value="Nagaland">Nagaland</option>
+                                                    <option value="Odisha">Odisha</option>
+                                                    <option value="Punjab">Punjab</option>
+                                                    <option value="Rajasthan">Rajasthan</option>
+                                                    <option value="Sikkim">Sikkim</option>
+                                                    <option value="Tamil Nadu">Tamil Nadu</option>
+                                                    <option value="Telangana">Telangana</option>
+                                                    <option value="Tripura">Tripura</option>
+                                                    <option value="Uttar Pradesh">Uttar Pradesh</option>
+                                                    <option value="Uttarakhand">Uttarakhand</option>
+                                                    <option value="West Bengal">West Bengal</option>
+
+                                                    <!-- Union Territories -->
+                                                    <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+                                                    <option value="Chandigarh">Chandigarh</option>
+                                                    <option value="Dadra and Nagar Haveli and Daman and Diu">
+                                                        Dadra and Nagar Haveli and Daman and Diu
+                                                    </option>
+                                                    <option value="Delhi">Delhi</option>
+                                                    <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+                                                    <option value="Ladakh">Ladakh</option>
+                                                    <option value="Lakshadweep">Lakshadweep</option>
+                                                    <option value="Puducherry">Puducherry</option>
                                                 </select>
                                             </div>
                                             <div class="ferr-msg" id="e-ec_state"><i
@@ -3348,55 +3425,147 @@
 
         // Note: Assuming jQuery is loaded in frontend.app
         if(typeof $ !== 'undefined') {
-            $('#state').on('change', function () {
-                let state = $(this).val();
-                $('#district').html('<option value="">Loading...</option>');
+            // $('#state').on('change', function () {
+            //     let state = $(this).val();
+            //     $('#district').html('<option value="">Loading...</option>');
 
-                if (state) {
-                    let url = "{{ route('get.districts', ':state') }}";
-                    url = url.replace(':state', state);
+            //     if (state) {
+            //         let url = "{{ route('get.districts', ':state') }}";
+            //         url = url.replace(':state', state);
 
-                    $.ajax({
-                        url: url,
-                        type: 'GET',
-                        success: function (response) {
-                            let options = '<option value="" disabled selected>Select District</option>';
-                            response.forEach(function (district) {
-                                options += `<option value="${district}">${district}</option>`;
-                            });
-                            $('#district').html(options);
-                        }
-                    });
+            //         $.ajax({
+            //             url: url,
+            //             type: 'GET',
+            //             success: function (response) {
+            //                 let options = '<option value="" disabled selected>Select District</option>';
+            //                 response.forEach(function (district) {
+            //                     options += `<option value="${district}">${district}</option>`;
+            //                 });
+            //                 $('#district').html(options);
+            //             }
+            //         });
 
-                } else {
-                    $('#district').html('<option value="" disabled selected>Select District</option>');
-                }
-            });
+            //     } else {
+            //         $('#district').html('<option value="" disabled selected>Select District</option>');
+            //     }
+            // });
 
-            $('#c_state').on('change', function () {
-                let state = $(this).val();
-                $('#c_district').html('<option value="">Loading...</option>');
+            // $('#c_state').on('change', function () {
+            //     let state = $(this).val();
+            //     $('#c_district').html('<option value="">Loading...</option>');
 
-                if (state) {
-                    let url = "{{ route('get.districts', ':state') }}";
-                    url = url.replace(':state', encodeURIComponent(state));
+            //     if (state) {
+            //         let url = "{{ route('get.districts', ':state') }}";
+            //         url = url.replace(':state', encodeURIComponent(state));
 
-                    $.ajax({
-                        url: url,
-                        type: 'GET',
-                        success: function (response) {
-                            let options = '<option value="" disabled selected>Select District</option>';
-                            response.forEach(function (district) {
-                                options += `<option value="${district}">${district}</option>`;
-                            });
-                            $('#c_district').html(options);
-                        }
-                    });
+            //         $.ajax({
+            //             url: url,
+            //             type: 'GET',
+            //             success: function (response) {
+            //                 let options = '<option value="" disabled selected>Select District</option>';
+            //                 response.forEach(function (district) {
+            //                     options += `<option value="${district}">${district}</option>`;
+            //                 });
+            //                 $('#c_district').html(options);
+            //             }
+            //         });
 
-                } else {
-                    $('#c_district').html('<option value="" disabled selected>Select District</option>');
-                }
-            });
+            //     } else {
+            //         $('#c_district').html('<option value="" disabled selected>Select District</option>');
+            //     }
+            // });
         }
+
+        document.getElementById('state').addEventListener('change', async function () {
+
+            let state = this.value;
+            let districtSel = document.getElementById('district');
+
+            // reset
+            districtSel.innerHTML = '<option value  ="">Loading...</option>';
+
+            if (!state) {
+                districtSel.innerHTML = '<option value="">District</option>';
+                return;
+            }
+
+            try {
+                let response = await fetch('https://countriesnow.space/api/v0.1/countries/state/cities', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        country: "India",
+                        state: state
+                    })
+                });
+
+                let result = await response.json();
+
+                districtSel.innerHTML = '<option value="">District</option>';
+
+                if (result.data && result.data.length > 0) {
+                    result.data.forEach(function (district) {
+                        let opt = document.createElement('option');
+                        opt.value = district;
+                        opt.textContent = district;
+                        districtSel.appendChild(opt);
+                    });
+                } else {
+                    districtSel.innerHTML = '<option value="">No District Found</option>';
+                }
+
+            } catch (error) {
+                console.error(error);
+                districtSel.innerHTML = '<option value="">Error loading districts</option>';
+            }
+        });
+
+        document.getElementById('c_state').addEventListener('change', async function () {
+
+            let state = this.value;
+            let districtSel = document.getElementById('c_district');
+
+            // reset
+            districtSel.innerHTML = '<option value  ="">Loading...</option>';
+
+            if (!state) {
+                districtSel.innerHTML = '<option value="">District</option>';
+                return;
+            }
+
+            try {
+                let response = await fetch('https://countriesnow.space/api/v0.1/countries/state/cities', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        country: "India",
+                        state: state
+                    })
+                });
+
+                let result = await response.json();
+
+                districtSel.innerHTML = '<option value="">District</option>';
+
+                if (result.data && result.data.length > 0) {
+                    result.data.forEach(function (district) {
+                        let opt = document.createElement('option');
+                        opt.value = district;
+                        opt.textContent = district;
+                        districtSel.appendChild(opt);
+                    });
+                } else {
+                    districtSel.innerHTML = '<option value="">No District Found</option>';
+                }
+
+            } catch (error) {
+                console.error(error);
+                districtSel.innerHTML = '<option value="">Error loading districts</option>';
+            }
+        });
     </script>
 @endpush
